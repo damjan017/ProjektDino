f6662d3173b183a44327a0c864af0e2f4fff2de3
damjan017 <besarovic@gmail.com>
2026-07-18 20:30:46 +0200

    Design: booking.com-inspiertes Karten-Layout, neue Startseite mit Reiseziele-Galerie

--- controller/controller.home.php
+++ controller/controller.home.php
@@ -5,3 +5,17 @@ Core::setView("home", "view1");
 $istHotelier = isset(Core::$user->Gruppe_literal)
     && strcasecmp((string) Core::$user->Gruppe_literal, "Hotelier") === 0;
 Core::publish($istHotelier, "istHotelier");
+
+// Ein paar Reiseziele fuer die Galerie auf der Startseite (je Stadt eine Unterkunft mit Bild)
+$reiseziele = Unterkunft::query(
+    "SELECT u.Name, u.Bild, a.Ortschaft
+     FROM Unterkunft u
+     LEFT JOIN Adresse a ON u._Adresse = a.id
+     WHERE u.Bild IS NOT NULL AND u.Bild != ''
+     GROUP BY a.Ortschaft
+     ORDER BY u.Bewertung DESC
+     LIMIT 8",
+    [],
+    true
+);
+Core::publish($reiseziele, "reiseziele");
