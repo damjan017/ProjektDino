942ac505dcbfdfa989573a706cd9c21f903d24dc
Damjan <besarovic@gmail.com>
2026-07-20 11:08:08 +0200

    Cleanup: Workshop-Reste entfernt, Lieferservice-Texte ersetzt, Debug-Ausgaben deaktiviert

--- models/model.Unterkunft_Ausstattung.scheme.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dataScheme=[
-"id"=>["type"=>"primary"],
-"m_ts"=>["type"=>"timestamp","format"=>"d.m.Y H:i:s"],
-"c_ts"=>["type"=>"timestamp","format"=>"d.m.Y H:i:s"],
-"_Unterkunft_a"=>["type"=>"integer","stereotyp"=>"primitive"],
-"_Ausstattung_b"=>["type"=>"integer","stereotyp"=>"primitive"],
-];
